7e8c3219ae1d64d995e49dc420e6d99c78ff3f11
Abdullah <abdullahbelal102938@gmail.com>
2025-08-05 10:43:37 +0200

    kk

--- app/utils/fonts/main.font.ts
+++ app/utils/fonts/main.font.ts
@@ -1,3 +1,11 @@
 import { Cairo } from "next/font/google";
 
-export const cairo = Cairo({ subsets: ["latin"], weight: "400" });
+export const cairo = Cairo({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+  preload: true,
+  fallback: ["Arial", "sans-serif"],
+  adjustFontFallback: true,
+  variable: "--font-cairo",
+});
